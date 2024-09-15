5a9e9e1efa78077cab0baeb020224a92dedd910e
zyzdai <73833115+zyzdai@users.noreply.github.com>
2024-09-16 01:48:32 +0800

    更新 tts.js

--- tts.js
+++ tts.js
@@ -5,12 +5,24 @@ module.exports = async (request, response) => {
   let voiceName = request.query["voiceName"] ?? "zh-CN-XiaoxiaoNeural"
   let style = request.query["style"] ?? "cheerful"
   let role = request.query["role"] ?? "Boy"
+  let rate = request.query["rate"] ?? "0"
+  let styledegree = request.query["styledegree"] ?? "1.5"
+  let pitch = request.query["pitch"] ?? "0"
   let text = request.query["text"] ?? ""
-
   try {
     let format = request.headers["format"] || "audio-24khz-48kbitrate-mono-mp3"
-    let ssml = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" xml:lang="zh-CN"><voice name="${voiceName}"><s /><mstts:express-as role="${role}" style="${style}">${text}</mstts:express-as><s /></voice></speak>`
-
+    let ssml = `<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts"
+xmlns:emo="http://www.w3.org/2009/10/emotionml" xml:lang="zh-CN">
+	<voice name="${voiceName}">
+		<s />
+		<mstts:express-as role="${role}" style="${style}" styledegree="${styledegree}">
+      <prosody rate="${rate}%" pitch="${pitch}%">
+				${text}
+			</prosody>
+		</mstts:express-as>
+		<s />
+	</voice>
+</speak>`
     let result = await serviceTTS.convert(ssml, format)
 
     response.sendDate = true
