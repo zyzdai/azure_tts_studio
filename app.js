44a621bdec1d3aa4ae5f4fc312ee14f7dd25e5cb
zyzdai <73833115+zyzdai@users.noreply.github.com>
2024-10-17 19:34:18 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
-app.get('/tts', require('./tts'))
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+// app.get('/tts', require('./tts'))
+
+app.post('/tts', require('./tts'))
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
